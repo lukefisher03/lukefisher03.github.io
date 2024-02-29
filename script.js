d05fd206bd34d466ccc7bc6f2079d6f90935ecde
Luke Fisher <lukedavidfisher03@gmail.com>
2024-02-29 11:23:40 -0500

    sidebar toggling

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-console.log("Hello")
+console.log("Hello, welcome to my mind")
+
 
 function testing() {
     console.log("Clicked me!")
+}
+
+
+function toggleSidebar() {
+    let sidebar = document.getElementsByClassName("sidebar")[0]
+    sidebar.style.display = sidebar.style.display == "none" ? "block" : "none"
 }
